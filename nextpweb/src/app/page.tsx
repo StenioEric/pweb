13cc16f705478f52c7e794bbb0138a4ae6f4bc36
StenioEric <stenioeric@gmail.com>
2025-01-17 10:21:36 -0300

    Pagina inicial com rotas para as receitas

--- nextpweb/src/app/page.tsx
+++ nextpweb/src/app/page.tsx
@@ -4,25 +4,68 @@ export default function Page() {
   return (
     <div
       style={{
-        fontFamily: "Arial, sans-serif",
+        fontFamily: "Verdana, sans-serif",
         textAlign: "center",
-        padding: "20px",
+        // backgroundColor: "#f4f4f4",
+        padding: "40px 20px",
+        minHeight: "80vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <h1>Receitas Incríveis</h1>
-      <p>Escolha uma receita para explorar:</p>
-      <ul style={{ listStyle: "none", padding: 0 }}>
+      <h1
+        style={{
+          fontSize: "36px",
+          color: "#333",
+          marginBottom: "20px",
+        }}
+      >
+        Receitas da disciplina de Programação Web
+      </h1>
+      <p
+        style={{
+          fontSize: "20px",
+          color: "#555",
+          marginBottom: "30px",
+        }}
+      >
+        Escolha uma receita para explorar:
+      </p>
+      <ul
+        style={{
+          listStyle: "none",
+          padding: 0,
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+          gap: "20px",
+          width: "100%",
+          maxWidth: "800px",
+        }}
+      >
         {Array.from({ length: 12 }, (_, index) => (
-          <li key={index} style={{ margin: "10px 0" }}>
+          <li key={index}>
             <Link
-              href={`./Receita${index + 1}/layout.tsx`}
-              style={{
-                textDecoration: "none",
-                color: "#0070f3",
-                fontSize: "18px",
-              }}
+              href={`https://github.com/StenioEric/pweb/blob/main/nextpweb/src/app/Receita${index + 1}`}
             >
-              Receita{index + 1}
+              <button
+                style={{
+                  backgroundColor: "#0070f3",
+                  color: "#fff",
+                  border: "none",
+                  borderRadius: "8px",
+                  padding: "15px 20px",
+                  fontSize: "16px",
+                  fontWeight: "bold",
+                  cursor: "pointer",
+                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+                  transition: "transform 0.2s, box-shadow 0.2s",
+                  width: "100%",
+                }}
+              >
+                Receita {index + 1}
+              </button>
             </Link>
           </li>
         ))}
